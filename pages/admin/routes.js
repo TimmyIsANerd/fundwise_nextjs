47b2602b2efe382f921ab8bd30212ba0ea6ed556
Solomon chika <Solomon.chika1@gmail.com>
2022-08-24 11:10:13 +0100

    Dashboard Routes commit

---
+++ pages/admin/routes.js
@@ -0,0 +1,45 @@
+var routes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: "ni ni-tv-2 text-primary",
+    layout: "/adminControl",
+  },
+  {
+    path: "/icons",
+    name: "Icons",
+    icon: "ni ni-planet text-blue",
+    layout: "/adminControl",
+  },
+  {
+    path: "/maps",
+    name: "Maps",
+    icon: "ni ni-pin-3 text-orange",
+    layout: "/adminControl",
+  },
+  {
+    path: "/profile",
+    name: "User Profile",
+    icon: "ni ni-single-02 text-yellow",
+    layout: "/adminControl",
+  },
+  {
+    path: "/tables",
+    name: "Tables",
+    icon: "ni ni-bullet-list-67 text-red",
+    layout: "/adminControl",
+  },
+  {
+    path: "/login",
+    name: "Login",
+    icon: "ni ni-key-25 text-info",
+    layout: "/auth",
+  },
+  {
+    path: "/register",
+    name: "Register",
+    icon: "ni ni-circle-08 text-pink",
+    layout: "/auth",
+  },
+];
+export default routes;
---
+++ pages/user/routes.js
@@ -0,0 +1,45 @@
+var routes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: "ni ni-tv-2 text-primary",
+    layout: "/account",
+  },
+  {
+    path: "/icons",
+    name: "Icons",
+    icon: "ni ni-planet text-blue",
+    layout: "/account",
+  },
+  {
+    path: "/maps",
+    name: "Maps",
+    icon: "ni ni-pin-3 text-orange",
+    layout: "/account",
+  },
+  {
+    path: "/profile",
+    name: "User Profile",
+    icon: "ni ni-single-02 text-yellow",
+    layout: "/account",
+  },
+  {
+    path: "/tables",
+    name: "Tables",
+    icon: "ni ni-bullet-list-67 text-red",
+    layout: "/account",
+  },
+  // {
+  //   path: "/login",
+  //   name: "Login",
+  //   icon: "ni ni-key-25 text-info",
+  //   layout: "/auth",
+  // },
+  // {
+  //   path: "/register",
+  //   name: "Register",
+  //   icon: "ni ni-circle-08 text-pink",
+  //   layout: "/auth",
+  // },
+];
+export default routes;
